ce2301efbd684b299ce6ad4db9092773b252f4d8
yya <lygs72656@163.com>
2020-11-26 18:35:43 +0800

    add file

---
+++ src/main/java/com/yangyuang/service/LoadFile.java
@@ -0,0 +1,5 @@
+package com.yangyuang.service;
+
+public interface LoadFile {
+
+}
---
+++ src/main/java/com/yangyuang/service/impl/LoadFileImpl.java
@@ -0,0 +1,6 @@
+package com.yangyuang.service.impl;
+
+import com.yangyuang.service.LoadFile;
+
+public class LoadFileImpl implements LoadFile {
+}
